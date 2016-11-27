10c4b8f677c78f8a86347e3d64bc3e66e268fcc6
Atsushi Togo <atz.togo@gmail.com>
2016-11-27 17:43:00 +0900

    Let setup.py compile directly using the source code but not using ALM library.

--- python/setup.py
+++ python/setup.py
@@ -4,15 +4,52 @@ from setuptools import setup, Extension
 import numpy
 include_dirs_numpy = [numpy.get_include()]
 
+compile_with_sources = True
+
+extra_link_args = ['-lstdc++', '-lgomp', '-llapack']
+library_dirs = []
+if compile_with_sources:
+    sources = ['alm.cpp',
+               'alm_core.cpp',
+               'alm_cui.cpp',
+               'constraint.cpp',
+               'error.cpp',
+               'fcs.cpp',
+               'files.cpp',
+               'fitting.cpp',
+               'input_parser.cpp',
+               'input_setter.cpp',
+               'interaction.cpp',
+               'main.cpp',
+               'patterndisp.cpp',
+               'symmetry.cpp',
+               'system.cpp',
+               'timer.cpp',
+               'writer.cpp']
+    if os.path.exists('src'):
+        source_dir = "src"
+    else:
+        source_dir = "../src"
+    
+    include_dirs = [source_dir,]
+    include_dirs += include_dirs_numpy
+    for i, s in enumerate(sources):
+        sources[i] = "%s/%s" % (source_dir, s) 
+    sources += ['_alm.c', 'alm_wrapper.cpp']
+else: # compile with library
+    sources = ['_alm.c', 'alm_wrapper.cpp']
+    # static link library
+    extra_link_args += ['../lib/libalmcxx.a']
+    # dynamic link library
+    # extra_link_args += ['-lalmcxx']
+    # library_dirs = ['../lib']
+
 extension = Extension('alm._alm',
                       include_dirs=include_dirs_numpy,
-                      library_dirs=["../lib"],
-                      extra_compile_args = ['-fopenmp'],
-                      extra_link_args=['-lstdc++',
-                                       '../lib/libalmcxx.a',
-                                       '-lgomp',
-                                       '-llapack'],
-                      sources=['_alm.c', 'alm_wrapper.cpp'])
+                      library_dirs=library_dirs,
+                      extra_compile_args = ['-fopenmp', '-std=c++11'],
+                      extra_link_args=extra_link_args,
+                      sources=sources)
 
 setup(name='alm',
       version='0.9.8',
