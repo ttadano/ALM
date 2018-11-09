f40a795dc103a68b49efcabaa04a33ee00d2eecf
Terumasa TADANO <terumasa.tadano@gmail.com>
2018-11-09 19:51:30 +0900

    Minor change of conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'python', 'alm'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 # -- General configuration ------------------------------------------------
 
