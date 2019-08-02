fe88d6ed176e6429803f023a224e622cbadd6ad7
Atsushi Togo <atz.togo@gmail.com>
2019-08-02 20:15:09 +0900

    Set compile_with_sources = True in setup.py

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ except ImportError:
     home = os.path.expanduser("~")
 
 # This is the switch for ALM developement. Always True for general cases.
-compile_with_sources = False
+compile_with_sources = True
 
 # Configurations to pass to extention.
 # The following directory structure and use of conda are supposed.
