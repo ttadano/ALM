067a8f5f4d945ac489adcbbc459b608446d5957a
Atsushi Togo <atz.togo@gmail.com>
2019-08-22 16:45:02 +0900

    permutation keyword in get_fc of python module is changed to have boolean type and the default is changed to True.

--- example/Si_fitting2.py
+++ example/Si_fitting2.py
@@ -96,7 +96,7 @@ with ALM(lavec, xcoord, kd) as alm:
         c2 = elem[1] % 3
         print("%f %d%s %d%s" % ((fc, v1 + 1, c[c1], v2 + 1, c[c2])))
 
-    fc_values, elem_indices = alm.get_fc(2, permutation=1)  # cubic fc
+    fc_values, elem_indices = alm.get_fc(2)  # cubic fc
     for (fc, elem) in zip(fc_values, elem_indices):
         v1 = elem[0] // 3
         c1 = elem[0] % 3
