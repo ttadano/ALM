7e60cf42e1abddccb3c8d5a6f4078b7f911f9f05
Atsushi Togo <atz.togo@gmail.com>
2019-08-22 15:09:06 +0900

    Updated python examples

--- example/lasso/LASSO_example.py
+++ example/lasso/LASSO_example.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import math
 import numpy as np
 from alm import ALM
 from sklearn.model_selection import GroupKFold
@@ -8,6 +7,7 @@ from sklearn.linear_model import lasso_path
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Lasso
 import matplotlib.pyplot as plt
+
 
 def get_crystal_structure():
     lavec = [[20.406, 0, 0],
@@ -89,40 +89,42 @@ def get_data(ndata, nat):
     return force, disp
 
 
-def run_alm(crystal, disp, force, maxorder=None, cutoff=None, nbody=None):
+def get_training_data(crystal, disp, force,
+                      maxorder=None, cutoff=None, nbody=None):
+    print("Preparing training data using ALM")
     with ALM(*crystal) as alm:
-        alm.set_verbosity(1)
+        alm.verbosity = 0
         alm.define(maxorder, cutoff, nbody)
         alm.set_constraint(translation=True)
-        alm.set_training_data(disp, force)
+        alm.displacements = disp
+        alm.forces = force
         X, y = alm.get_matrix_elements()
     return X, y
 
 
-def run_scikit_learn(X, y, nat,
-                     force_ravel,
-                     n_alphas=100,
-                     eps=1.0e-7,
-                     n_splits=5,
-                     standardize=True):
+def run_lasso_by_scikit_learn(X, y, nat, ndata,
+                              force_ravel,
+                              n_alphas=100,
+                              eps=1.0e-7,
+                              n_splits=5,
+                              standardize=True):
+    print("Start training by scikit-learn")
     print("force shape: ", np.shape(force_ravel))
     print("Amat shape : ", np.shape(X))
 
-    groups = []
-    for i in range(30):
-        groups.extend([i] * 3 * nat)
+    groups = np.repeat(np.arange(ndata), 3 * nat)
 
     gkf = GroupKFold(n_splits=n_splits)
 
     rms_train = np.zeros((n_alphas, n_splits))
     rms_test = np.zeros((n_alphas, n_splits))
 
-    counter = 0
     alphas = np.logspace(-1, -6, num=n_alphas)
 
     sc = StandardScaler()
 
-    for train, test in gkf.split(X, y, groups=groups):
+    for i, (train, test) in enumerate(gkf.split(X, y, groups=groups)):
+        print("KFold CV %d / %d" % (i + 1, n_splits))
 
         force_train = force_ravel[train]
         force_test = force_ravel[test]
@@ -142,7 +144,7 @@ def run_scikit_learn(X, y, nat,
 
         alphas_lasso, coefs_lasso, _ = lasso_path(X_train, y_train, eps=eps,
                                                   alphas=alphas,
-                                                  verbose=True,
+                                                  verbose=False,
                                                   fit_intercept=False)
 
         y_model_train = np.dot(X_train, coefs_lasso)
@@ -155,10 +157,8 @@ def run_scikit_learn(X, y, nat,
             residual_train = np.dot(y_diff_train, y_diff_train)
             residual_test = np.dot(y_diff_test, y_diff_test)
 
-            rms_train[ialpha, counter] = math.sqrt(residual_train/fnorm_train)
-            rms_test[ialpha, counter] = math.sqrt(residual_test/fnorm_test)
-
-        counter += 1
+            rms_train[ialpha, i] = np.sqrt(residual_train / fnorm_train)
+            rms_test[ialpha, i] = np.sqrt(residual_test / fnorm_test)
 
     rmse_mean = np.mean(rms_train, axis=1)
     rmse_std = np.std(rms_train, axis=1, ddof=1)
@@ -175,7 +175,7 @@ def run_scikit_learn(X, y, nat,
     np.savetxt('sklearn_cvscore.dat', data)
 
     alpha_opt = alphas[np.argmin(cv_mean)]
-    print(alpha_opt)
+    print("Optimal alpha: %e" % alpha_opt)
     clf = Lasso(alpha=alpha_opt, fit_intercept=False, tol=eps)
     if standardize:
         scaler = sc.fit(X)
@@ -195,27 +195,27 @@ def main():
     ndata = 30
     force, disp = get_data(ndata, nat)
     maxorder = 5
-    cutoff = [-1, -1, 15, 8, 8]
+    cutoff = [[[-1]], [[-1]], [[15]], [[8]], [[8]]]
     nbody = [2, 3, 3, 2, 2]
-    X, y = run_alm(crystal, disp, force,
-                   maxorder=maxorder, cutoff=cutoff, nbody=nbody)
+    X, y = get_training_data(crystal, disp, force,
+                             maxorder=maxorder, cutoff=cutoff, nbody=nbody)
 
-    n_alphas = 300
+    n_alphas = 100
     eps = 1.0e-7
     n_splits = 5
     standardize = True
-    fc, alphas_lasso, rmse_mean, cv_mean = run_scikit_learn(
-        X, y, nat,
+    fc, alphas_lasso, rmse_mean, cv_mean = run_lasso_by_scikit_learn(
+        X, y, nat, ndata,
         force.ravel(),
         n_alphas=n_alphas,
         eps=eps,
         n_splits=n_splits,
         standardize=standardize)
 
-    print(rmse_mean)
-    print(cv_mean)
-    print(fc)
-    print(np.linalg.norm(fc, 0))
+    # print(rmse_mean)
+    # print(cv_mean)
+    # print(fc)
+    # print(np.linalg.norm(fc, 0))
 
     with ALM(*crystal) as alm:
         alm.define(maxorder, cutoff, nbody)
@@ -223,20 +223,25 @@ def main():
         alm.set_fc(fc)
         fc_values1, elem_indices1 = alm.get_fc(1, mode='origin')
 
-    c = "xyz"
-    for (val, elem) in zip(fc_values1, elem_indices1):
-        v1 = elem[0] // 3
-        c1 = elem[0] % 3
-        v2 = elem[1] // 3
-        c2 = elem[1] % 3
-        print("%15.7f %d%s %d%s" % ((val, v1 + 1, c[c1], v2 + 1, c[c2])))
+    with open("fc-LASSO.dat", 'w') as w:
+        c = "xyz"
+        for (val, elem) in zip(fc_values1, elem_indices1):
+            v1 = elem[0] // 3
+            c1 = elem[0] % 3
+            v2 = elem[1] // 3
+            c2 = elem[1] % 3
+            w.write("%15.7f %d%s %d%s\n" %
+                    ((val, v1 + 1, c[c1], v2 + 1, c[c2])))
+    print("Force constants data are written in fc-LASSO.dat")
 
-    ax = plt.subplot(111)
+    fig, ax = plt.subplots()
     ax.plot(alphas_lasso, rmse_mean, linestyle='--', marker='o', ms=5)
     ax.plot(alphas_lasso, cv_mean, linestyle='--', marker='o', ms=5)
     ax.set_xscale('log')
 
-    plt.show()
+    fig_filename = "LASSO-example.png"
+    plt.savefig(fig_filename)
+    print("Training and test RMSE vs alpha are plotted in %s." % fig_filename)
 
 
 if __name__ == '__main__':
